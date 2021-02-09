be1bd9b821a92da3e72af72e2fe50cf6e2e41d50
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-09 16:46:48 -0300

    feat: add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,16 @@
-const App = () => <div className="App"></div>;
+import './app.scss';
+import { Router } from '@reach/router';
+import { Details } from 'pages/details';
+import { Login } from 'pages/login';
+import Main from 'pages/main/Main';
+import { Signup } from 'pages/signup';
+
+const App = () => (
+  <Router>
+    <Signup path="/signup" />
+    <Login path="/login" />
+    <Details path="/details" />
+    <Main default />
+  </Router>
+);
 export default App;
