0f59c721f9a99d8d1806d9aeb7c87014d134aec6
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-10 16:23:58 -0300

    feat: add react query provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import { Details } from 'pages/details';
 import { Login } from 'pages/login';
 import Main from 'pages/main/Main';
 import { Signup } from 'pages/signup';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => (
-  <Router>
-    <Signup path="/signup" />
-    <Login path="/login" />
-    <Details path="/details" />
-    <Main default />
-  </Router>
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <Signup path="/signup" />
+      <Login path="/login" />
+      <Details path="/details" />
+      <Main default />
+    </Router>
+  </QueryClientProvider>
 );
 export default App;
