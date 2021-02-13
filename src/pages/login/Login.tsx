1c5222f29175804328a5591018c5f850673ef47e
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-12 22:36:42 -0300

    feat: add login page to router

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,12 @@
+import { RouteComponentProps } from '@reach/router';
+import { Sign } from 'components/sign';
+import { useDocTitle } from 'custom-hooks/use-doc-title';
+import './login.scss';
+
+const Login = (_props: RouteComponentProps) => {
+  useDocTitle('Consulta de CNPJ | Login');
+
+  return <Sign type="login" />;
+};
+
+export default Login;
